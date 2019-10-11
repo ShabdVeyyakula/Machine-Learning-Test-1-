bc77d44d0079a517e87dbc062bed7daff91b0f34
ShabdVeyyakula <47516044+ShabdVeyyakula@users.noreply.github.com>
2019-10-11 10:13:48 -0700

    Add files via upload
    This is the Python File itself.  This corresponds with the different pathway python text.

---
+++ machine_learning.py
@@ -0,0 +1,11 @@
+#data reprocessing
+
+#importing libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the DataSet
+
+dataset = pd.read_csv('Machine-Learning-Test-1--master\Data.csv')
